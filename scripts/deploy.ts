e476578804db5c4935b7476b3c890aac01586e91
Bachir ARIF <arif.bachir@hotmail.com>
2023-01-27 14:59:11 +0100

    Remove idea folder

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,14 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const globalConfig = require("../config.json");
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  console.log("Start deploy");
+  const MevProtocol = await ethers.getContractFactory("MevProtocol");
+  const mevProtocol = await MevProtocol.deploy(globalConfig.tokens);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  await mevProtocol.deployed();
+  console.log("Mev Protocole deployed to:", mevProtocol.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
